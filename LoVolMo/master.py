95fd3fe26c2310cee88dc832cadaadcce94c63d2
Gabriel Wendell <67518461+GabrielWendell@users.noreply.github.com>
2022-10-16 17:35:46 -0300

    Master file for LoVolMo.
    Master file with the core of the LoVolMo package.

---
+++ LoVolMo/master.py
@@ -0,0 +1,10 @@
+import methods as LV
+
+print('#-###################################################')
+print('Initiating the Lotka-Volterra Models package')
+print('#-###################################################')
+
+
+#LV.TwoSpeciesModel()
+#LV.ThreeSpeciesModel()
+#LV.HastingModel()
