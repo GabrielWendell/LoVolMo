aefc0f4f18e3e542600745056cdcd0c56c569577
Gabriel Wendell <67518461+GabrielWendell@users.noreply.github.com>
2022-10-16 17:24:39 -0300

    Package folder.
    Lotka-Volterra Models package folder.

---
+++ LoVolMo/master.py
@@ -0,0 +1,10 @@
+import Lotka_Volterra_aux_v2 as LV
+
+print('#-###################################################')
+print('Initiating the Lotka-Volterra Models package')
+print('#-###################################################')
+
+
+#LV.TwoSpeciesModel()
+#LV.ThreeSpeciesModel()
+#LV.HastingModel()
